e217ce90f565d27ef7040d1ab0594e74a8d7d71e
ichitaso <ichitaso@yahoo.co.jp>
2020-11-10 00:43:37 +0900

    Fix: kernbypassd doesn't work

--- kernbypassd/kernbypassd.m
+++ kernbypassd/kernbypassd.m
@@ -13,14 +13,43 @@ static void easy_spawn(const char *args[]) {
     waitpid(pid, &status, WEXITED);
 }
 
+#define FLAG_PLATFORMIZE (1 << 1)
+
+void platformize_me() {
+    void *handle = dlopen("/usr/lib/libjailbreak.dylib", RTLD_LAZY);
+    if (!handle) return;
+    // Reset errors
+    dlerror();
+    typedef void (*fix_entitle_prt_t)(pid_t pid, uint32_t what);
+    fix_entitle_prt_t ptr = (fix_entitle_prt_t)dlsym(handle, "jb_oneshot_entitle_now");
+
+    const char *dlsym_error = dlerror();
+    if (dlsym_error) return;
+
+    ptr(getpid(), FLAG_PLATFORMIZE);
+}
+
+void patch_setuid() {
+    void *handle = dlopen("/usr/lib/libjailbreak.dylib", RTLD_LAZY);
+    if (!handle) return;
+    // Reset errors
+    dlerror();
+    typedef void (*fix_setuid_prt_t)(pid_t pid);
+    fix_setuid_prt_t ptr = (fix_setuid_prt_t)dlsym(handle, "jb_oneshot_fix_setuid_now");
+
+    const char *dlsym_error = dlerror();
+    if (dlsym_error) return;
+
+    ptr(getpid());
+}
+
 int main(int argc, char **argv, char **envp) {
     @autoreleasepool {
-        // root
+        patch_setuid();
+        platformize_me();
         setuid(0);
-        setgid(0);
-        if (getuid() != 0 && getgid() != 0) {
-            printf("Require kernbypassd to be run as root!\n");
-            return -1;
+        if ((chdir("/")) < 0) {
+            exit(EXIT_FAILURE);
         }
 
         if (autoEnabled && access(kernbypassMem, F_OK) != 0) {
